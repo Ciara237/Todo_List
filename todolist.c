42c1d2bafe7868beaf6561dae1a9612877c687d2
Ciara237 <149544940+Ciara237@users.noreply.github.com>
2023-12-07 03:08:24 +0100

    todolist.c

--- todolist.c
+++ todolist.c
@@ -2,13 +2,12 @@
 #include<string.h>
 
 #define MAX_LENGTH 10
-#define MAX_LIST 100
 
 int i, numread;
 //Creates a stucture List which contains a name and the various task on the list.
 struct List{
     char task[MAX_LIST];
-    char date[MAX_LENGTH];
+    char tasknum[MAX_LENGTH];
 };
 
 int numtask = 0;//Crates a global variable numtask that keeps track of the number of task entered by the user.
@@ -44,7 +43,7 @@ void viewlist(struct List tasks[], int taskcount){
     }
     else{
         for( i = 1 ; i <= numtask ; i++){
-               printf("%d. %s\n", i, tasks[i-1].task);//Displays each task
+               printf("%d. %s\n", tasks[i-1].tasknum, tasks[i-1].task);//Displays each task
            };
     }
 }
@@ -59,7 +58,7 @@ void deletetask(struct List tasks[], int taskcount, int index){
     do{
     printf("Enter the index of the task you want to delete: ");
     numread = scanf("%d", &index);
-    validitycheckint(numread, index);
+    validitycheckint(numread, index);//Checks if the input for index is an integer value.
     }while(numread == 0);
 
     if (index < 1 || index > numtask) {//Checks if the notes's index requested for is actually in the range of existing notes
@@ -76,6 +75,7 @@ void deletetask(struct List tasks[], int taskcount, int index){
 }
 
 int main(){
+    //Declare the variables to hold the user and list name.
     char listname[MAX_LENGTH];
     char name[MAX_LENGTH];
 
@@ -98,36 +98,37 @@ int main(){
     printf("2. view List\n");
     printf("3. Delete task\n");
     printf("4. Exit\n");
-    printf("WHat operation do you want to perform on List %s?\n", listname);
+    printf("What operation do you want to perform on List %s?\n", listname);
     numread = scanf("%d", &option);
-    validitycheckint(numread, option);
+    validitycheckint(numread, option);//Checks if the input for option is an integer value.
     }while(numread == 0);
 
-    struct List tasks[MAX_LENGTH];
-    int taskcount = 0;
+    struct List tasks[MAX_LENGTH];//Declare a variable tasks.
+    int taskcount = 0;//Sets the initial number of tasks to zero.
 
     switch (option){
         case 1:
-            addtask(tasks, taskcount);
+            addtask(tasks, taskcount);//Calls the addtask function.
         break;
         case 2:
             printf("%s List\n",listname );
-            viewlist(tasks, taskcount);
+            viewlist(tasks, taskcount);//Calls the viewlist function
         break;
         case 3:
-            deletetask(tasks, taskcount, index);
+            deletetask(tasks, taskcount, index);//Calls the deletetask function
         break;
         case 4:
+            
             printf("Exiting...\n");
             printf("Thank you for using my Application!!\n");
         break;
         default:
-            if( option < 1 || option > 4){
+            if( option < 1 || option > 4){//Displays an error message if the requested option is not within range
                 printf("Invalid choice. Make another one.");
                 goto s;
             };
     }
-    system("pause");
+    system("pause");//Allows the user to view the output before running another instance of the code
     }while( option != 4 );
     return 0;
 }
